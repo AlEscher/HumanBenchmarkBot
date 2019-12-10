0950446c0e2563a10b902e01d2a7aa728cde58a1
Kekz <pinco-pallino@gmx.net>
2019-12-10 12:57:29 +0100

    Still some issues with tesseract
    Works better for longer numbers.
    Now converting the image to black text on white background
    Some consistent mistakes like reading "2" instead of "5" that I can't do much about

--- numbermemory.py
+++ numbermemory.py
@@ -10,10 +10,10 @@ myKeyboard = pynput.keyboard.Controller()
 myMouse = pynput.mouse.Controller()
 screensize = windll.user32.GetSystemMetrics(
     0), windll.user32.GetSystemMetrics(1)
-# path to tesseract's executable, this is should be the standard path
+# path to tesseract's executable, this should be the standard path
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 image = None
-limit = 1
+limit = 5
 
 if (screensize[0] == 1920 and screensize[1] == 1080):
     sys.exit(1)
@@ -27,30 +27,46 @@ else:
 
 time.sleep(0.2)
 
-for x in range(0, limit):
+for lvl in range(0, limit):
     if (screensize[0] == 1920 and screensize[1] == 1080):
         sys.exit(1)
     elif (screensize[0] == 2560 and screensize[1] == 1440):
-        image = ImageGrab.grab(bbox=(12, 260, 2446, 560))
+        image = ImageGrab.grab(bbox=(1200, 300, 1346, 520))
 
     if (image is not None):
         #print(pytesseract.image_to_string(image, config='digits'))
-        # image = image.convert("RGBA")
-        # pixeldata = image.load()
-        # #convert the blue background to black in order to help tesseract recognize the number
-        # for y in range(0, image.size[1]):
-        #     for x in range(0, image.size[0]):
-        #         if pixeldata[x, y] != (255, 255, 255, 255):
-        #             pixeldata[x, y] = (0, 0, 0, 255)
+        image = image.convert("RGBA")
+        pixeldata = image.load()
+        #convert the blue background to white and number to black in order to help tesseract recognize the number
+        for y in range(0, image.size[1]):
+            for x in range(0, image.size[0]):
+                if pixeldata[x, y] != (255, 255, 255, 255):
+                    pixeldata[x, y] = (250, 250, 250, 255)
+                else:
+                    pixeldata[x, y] = (0, 0, 0, 255)
         
-        number = pytesseract.image_to_string(image, lang="eng", config="--oem 0")
+        number = ""
+        if (lvl == 0):
+            # supposedly better psm (page segmentation mode) for single digits
+            number = pytesseract.image_to_string(image, config='--psm 10')
+        else:
+            # psm for single word
+            number = pytesseract.image_to_string(image, config='--psm 8')
+        #try and filter out some consistent mistakes made by tesseract
+        number = number.replace("i", "1").replace("&", "6")
         print(number)
         image.show()
-        time.sleep(1.5)
+        
+        if (not number.isdigit()):
+            print("Tesseract OCR failed to recognize the number")
+            sys.exit(1)
         # click on text feld to focus it
+        time.sleep(1)
         myMouse.click(pynput.mouse.Button.left, 1)
         myKeyboard.type(number)
         myKeyboard.press(pynput.keyboard.Key.enter)
         myKeyboard.release(pynput.keyboard.Key.enter)
+        time.sleep(0.1)
         myKeyboard.press(pynput.keyboard.Key.enter)
         myKeyboard.release(pynput.keyboard.Key.enter)
+        time.sleep(0.5)
