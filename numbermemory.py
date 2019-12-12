0413e44f56bc163efb218cf60ddea0bbf6456424
Alessandro <alessandroescher@gmx.net>
2019-12-12 12:23:48 +0100

    Working numbermemory

--- numbermemory.py
+++ numbermemory.py
@@ -13,7 +13,7 @@ screensize = windll.user32.GetSystemMetrics(
 # path to tesseract's executable, this should be the standard path
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 image = None
-limit = 5
+limit = 30
 
 if (screensize[0] == 1920 and screensize[1] == 1080):
     myMouse.position = (953, 574)
@@ -42,34 +42,37 @@ for lvl in range(0, limit):
         # convert the blue background to white and number to black in order to help tesseract recognize the number
         for y in range(0, image.size[1]):
             for x in range(0, image.size[0]):
-                if pixeldata[x, y] != (255, 255, 255, 255):
+                if pixeldata[x, y] < (250, 250, 250, 255):
                     pixeldata[x, y] = (255, 255, 255, 255)
                 else:
                     pixeldata[x, y] = (0, 0, 0, 255)
 
         number = ""
         if (lvl == 0):
-            # supposedly better psm (page segmentation mode) for single digits
+            # supposedly better psm (page segmentation mode) for single characters
             number = pytesseract.image_to_string(image, config='--psm 10')
         else:
-            # psm for single word
-            number = pytesseract.image_to_string(image, config='--psm 8')
+            # config for digits
+            number = pytesseract.image_to_string(image, config='digits')
         # try and filter out some consistent mistakes made by tesseract
         print("Tesseract read: " + number)
-        number = number.replace("i", "1").replace(
-            "&", "6").replace("t", "7").replace("a", "4")
-        print("Writing " + number)
+        if (not number.isdigit()):
+            number = number.replace("i", "1").replace(
+                "&", "6").replace("t", "7").replace("a", "4").replace("G", "6")
+
         # image.show()
 
         if (not number.isdigit() or len(number) != lvl + 1):
             print("Tesseract OCR failed to recognize the number")
             sys.exit(1)
 
+        print("Writing: " + number)
+
         delay = 2
         # click on text feld to focus it
         if (lvl >= 2):
-            delay += (lvl - 1) * 0.75
-        print("Waiting: " + str(delay))
+            delay += (lvl - 1) * 1
+        print("Waiting: " + str(delay) + "s")
         time.sleep(delay)
         myMouse.click(pynput.mouse.Button.left, 1)
         myKeyboard.type(number)
