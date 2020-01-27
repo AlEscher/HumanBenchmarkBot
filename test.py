344e16bab17decdd8fa422b97914e55815289a9d
AlEscher <50444010+AlEscher@users.noreply.github.com>
2020-01-27 12:16:23 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-driver = webdriver.Chrome(options = options, executable_path=r'C:\\Users\\Ale\\Downloads\\chromedriver_win32\\chromedriver.exe')
-driver.get('http://google.com/')
-print("Chrome Browser Invoked")
-driver.quit()
